53ecf6eff2e45fa4fd9e9fa63f63c16fd083845e
Klamt Lukas <lukas.klamt@ausy-technologies.de>
2022-11-22 16:19:32 +0100

    feat: add post form for store

--- lib/workshop_management_web/controllers/store_html.ex
+++ lib/workshop_management_web/controllers/store_html.ex
@@ -4,6 +4,7 @@ defmodule WorkshopManagementWeb.StoreHTML do
   def index(assigns) do
     ~H"""
     <h2 class="text-2xl font-bold underline my-5">Store</h2>
+    <.store_form />
     <ul class="my-5">
       <%= for {key, value} <- @store do %>
         <li><%= key %>: <%= value %></li>
@@ -11,6 +12,21 @@ defmodule WorkshopManagementWeb.StoreHTML do
     </ul>
     """
   end
+
+  defp store_form(assigns) do
+    ~H"""
+    <form class="grid items-center gap-2 grid-cols-4" action="/store" method="post">
+      <label class="text-right col-span-1" for="store_key">Key:</label>
+      <input class="rounded col-span-3" type="text" name="store_key" />
+      <label class="text-right col-span-1" for="store_value">Value:</label>
+      <input class="rounded col-span-3" type="text" name="store_value" />
+      <button
+        class="col-start-3 col-span-2 bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        type="submit"
+      >
+        Send
+      </button>
+    </form>
     """
   end
 end
